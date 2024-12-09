612714f80d41d1ea9c15264c04268d3e2a13fb7d
Muhammad Danish <md0145678@gmail.com>
2024-12-09 16:14:33 +0500

    updated imges

--- src/components/FoodCatergaery.tsx
+++ src/components/FoodCatergaery.tsx
@@ -14,7 +14,7 @@ const FoodCategory = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 shadow-md shadow-[#FF9F0D]/20">
             <Image
-              src="./food categary1-final.png"
+              src="./Food categary1-final.png"
               alt="Food item 1"
               width={280}
               height={300}
@@ -23,7 +23,7 @@ const FoodCategory = () => {
           </div>
           <div className="flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 shadow-md shadow-[#FF9F0D]/20">
             <Image
-              src="./food categary2-final.png"
+              src="./Food categary2-final.png"
               alt="Food item 2"
               width={280}
               height={300}
@@ -32,7 +32,7 @@ const FoodCategory = () => {
           </div>
           <div className="flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 shadow-md shadow-[#FF9F0D]/20">
             <Image
-              src="./food categary3.png"
+              src="./Food categary3.png"
               alt="Food item 3"
               width={280}
               height={300}
@@ -41,7 +41,7 @@ const FoodCategory = () => {
           </div>
           <div className="flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 shadow-md shadow-[#FF9F0D]/20">
             <Image
-              src="./food categary4.png"
+              src="./Food categary4.png"
               alt="Food item 4"
               width={280}
               height={300}
